5be152c401e4955a2a142c66e7f03679547f45e9
Kittipos Wajakajornrit <boss.zz@me.com>
2024-02-19 20:03:10 +0700

    Fix Init Script & Sharp Package

--- init_start.sh
+++ init_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=/app/cached/migrated
+FILE=/app/caches/migrated
 
 if test -f "$FILE"; then
     echo "Already migrated"
@@ -10,7 +10,8 @@ else
     npx prisma generate --schema=./prisma/schema.prisma
     npx prisma migrate reset --schema=./prisma/schema.prisma --force
     npx prisma migrate deploy --schema=./prisma/schema.prisma
-    rm -rf /app/cached/*
+    yarn add @img/sharp-linux-x64@0.33.2
+    rm -rf /app/caches/*
     yarn migrate
     echo "Migration complete"
     touch "$FILE"
