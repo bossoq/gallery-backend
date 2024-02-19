36162d8f8e84a9fd5b99b32651a72bac083ca28d
Kittipos Wajakajornrit <boss.zz@me.com>
2024-02-19 19:07:03 +0700

    Change Init Command

--- init_start.sh
+++ init_start.sh
@@ -8,7 +8,7 @@ if test -f "$FILE"; then
 else
     echo "Migrating..."
     npx prisma generate --schema=./prisma/schema.prisma
-    npx prisma migrate reset --schema=./prisma/schema.prisma
+    npx prisma migrate reset --schema=./prisma/schema.prisma --force
     npx prisma migrate deploy --schema=./prisma/schema.prisma
     rm -rf /app/cached/*
     yarn migrate
