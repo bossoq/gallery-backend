5c655d28dbda837328dc26e3e1398716ef120227
Kittipos Wajakajornrit <boss.zz@me.com>
2024-02-19 20:50:35 +0700

    Change Init Step

--- app_start.sh
+++ app_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+yarn set version stable
+yarn install
 yarn add @img/sharp-linux-x64@0.32.6
 yarn start
--- init_start.sh
+++ init_start.sh
@@ -6,11 +6,12 @@ if test -f "$FILE"; then
     echo "Already migrated"
 else
     echo "Migrating..."
+    yarn set version stable
+    yarn install
+    yarn add @img/sharp-linux-x64@0.33.2
     npx prisma generate --schema=./prisma/schema.prisma
     npx prisma migrate reset --schema=./prisma/schema.prisma --force
     npx prisma migrate deploy --schema=./prisma/schema.prisma
-    yarn set version stable
-    yarn add @img/sharp-linux-x64@0.33.2
     rm -rf /app/caches/*
     yarn migrate
     echo "Migration complete"
