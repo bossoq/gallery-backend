ff2828c9b2a3b75af0c25e0a7e4415440cf480b1
Kittipos Wajakajornrit <boss.zz@me.com>
2024-02-19 21:41:49 +0700

    Fix loop step

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,6 +1,5 @@
 import { randomUUID } from 'node:crypto'
-import { readdir } from 'node:fs/promises'
-import { readFileSync, writeFileSync } from 'node:fs'
+import { readdir, readFile, writeFile } from 'node:fs/promises'
 import { basename, extname, join } from 'node:path'
 import { createFileEntry } from './prismaUtils'
 import exif from 'exif-reader'
@@ -30,119 +29,57 @@ const scanDirectory = async (dirPath: string): Promise<unknown[]> =>
       .then((files) => files.flat(Infinity).filter((file) => file !== null))
   )
 
-// get metadata for a file
-// const getFileMetadata = async (filePath: string): Promise<FilestoreEntry> => {
-//   console.debug(`Processing ${filePath}`)
-//   console.debug(`Getting metadata for ${filePath}`)
-//   const fileMetadata = await sharp(`${filePath}`).metadata()
-//   const fileId = randomUUID()
-//   const fileName = basename(filePath)
-//   const fileDir = filePath
-//     .replace(`/${fileName}`, '')
-//     .replace(`${directory.replace('./', '')}/`, '')
-//   const fileSize = await sharp(`${filePath}`)
-//     .toBuffer()
-//     .then((buffer) => buffer.byteLength)
-//   const fileTags = [fileDir]
-//   let createdAt = new Date()
-//   if (fileMetadata.exif) {
-//     const { exif: exifData } = exif(fileMetadata.exif)
-//     if (exifData) {
-//       const { DateTimeOriginal } = exifData
-//       if (DateTimeOriginal) createdAt = DateTimeOriginal as Date
-//     }
-//   }
-//   const fileStore = {
-//     fileId,
-//     fileName,
-//     fileDir,
-//     fileType: `image/${fileMetadata.format}`,
-//     fileSize,
-//     fileTags,
-//     width: fileMetadata.width ?? 0,
-//     height: fileMetadata.height ?? 0,
-//     createdAt
-//   }
-//   console.debug(`Finished getting metadata for ${filePath}`)
-//   return fileStore
-// }
-
-// const createThumbnail = async (filePath: string, fileId: string) => {
-//   const fileBuffer = await sharp(`${filePath}`)
-//     .resize(200)
-//     .jpeg({ mozjpeg: true })
-//     .toBuffer()
-//   console.debug(`Writing thumbnail for ${filePath}`)
-//   await writeFile(`${caches}/${fileId}.jpg`, fileBuffer)
-//   console.debug(`Finished writing thumbnail for ${filePath}`)
-// }
-
-scanDirectory(directory)
-  .then((files) => {
-    for (const [idx, file] of files.flat(Infinity).entries()) {
-      console.log(
-        `Processing file ${idx + 1} of ${files.flat(Infinity).length}`
-      )
-      console.debug(`Processing ${file}`)
-      console.debug(`Getting metadata for ${file}`)
-      const fileBuff = readFileSync(file as string)
-      sharp(fileBuff)
-        .metadata()
-        .then((fileMetadata) => {
-          console.debug(`${file} metadata: ${JSON.stringify(fileMetadata)}`)
-          const fileId = randomUUID().toString()
-          const fileName = basename(file as string)
-          const fileDir = (file as string)
-            .replace(`/${fileName}`, '')
-            .replace(`${directory.replace('./', '')}/`, '')
-          sharp(fileBuff)
-            .toBuffer()
-            .then((buffer) => buffer.byteLength)
-            .catch((err) => console.error(err))
-            .then((fileSize) => {
-              console.debug(`${file} size: ${fileSize}`)
-              const fileTags = [fileDir]
-              let createdAt = new Date()
-              if (fileMetadata.exif) {
-                const { exif: exifData } = exif(fileMetadata.exif)
-                if (exifData) {
-                  const { DateTimeOriginal } = exifData
-                  if (DateTimeOriginal) createdAt = DateTimeOriginal as Date
-                }
-              }
-              const fileStore = {
-                fileId,
-                fileName,
-                fileDir,
-                fileType: `image/${fileMetadata.format}`,
-                fileSize,
-                fileTags,
-                width: fileMetadata.width ?? 0,
-                height: fileMetadata.height ?? 0,
-                createdAt
-              }
-              console.debug(`Finished getting metadata for ${file}`)
-              console.debug(`Writing thumbnail for ${file}`)
-              sharp(fileBuff)
-                .resize(200)
-                .jpeg({ mozjpeg: true })
-                .toBuffer()
-                .then((fileBuffer) => {
-                  writeFileSync(`${caches}/${fileId}.jpg`, fileBuffer)
-                  console.debug(`Finished writing thumbnail for ${file}`)
-                })
-                .catch((err) => console.error(err))
-              console.debug(`Writing metadata for ${file}`)
-              createFileEntry(fileStore as FilestoreEntry)
-                .then(() =>
-                  console.debug(`Finished writing metadata for ${file}`)
-                )
-                .catch((err) => console.error(err))
-              console.debug(`Finished processing ${file}`)
-            })
-            .catch((err) => console.error(err))
-        })
-        .catch((err) => console.error(err))
+// migrate files
+const migrate = async (directory: string) => {
+  const files = await scanDirectory(directory)
+  const flatFiles = files.flat(Infinity)
+  const totalFiles = flatFiles.length
+  for (const [idx, file] of flatFiles.entries()) {
+    console.log(`Processing file ${idx + 1} of ${totalFiles}`)
+    console.debug(`Processing ${file}`)
+    console.debug(`Getting metadata for ${file}`)
+    const fileBuff = await readFile(file as string)
+    const fileMetadata = await sharp(fileBuff).metadata()
+    console.debug(`${file} format: ${fileMetadata.format}`)
+    const fileId = randomUUID().toString()
+    const fileName = basename(file as string)
+    const fileDir = (file as string)
+      .replace(`/${fileName}`, '')
+      .replace(`${directory.replace('./', '')}/`, '')
+    const fileSize = fileBuff.byteLength
+    console.debug(`${file} size: ${fileSize} bytes`)
+    const fileTags = [fileDir]
+    let createdAt = new Date()
+    if (fileMetadata.exif) {
+      const { exif: exifData } = exif(fileMetadata.exif)
+      if (exifData) {
+        const { DateTimeOriginal } = exifData
+        if (DateTimeOriginal) createdAt = DateTimeOriginal as Date
+      }
     }
-  })
-  .catch((err) => console.error(err))
+    const fileStore = {
+      fileId,
+      fileName,
+      fileDir,
+      fileType: `image/${fileMetadata.format}`,
+      fileSize,
+      fileTags,
+      width: fileMetadata.width ?? 0,
+      height: fileMetadata.height ?? 0,
+      createdAt
+    }
+    console.debug(`Finished getting metadata for ${file}`)
+    console.debug(`Creating thumbnail for ${file}`)
+    const fileBuffer = await sharp(fileBuff)
+      .resize(200)
+      .jpeg({ mozjpeg: true })
+      .toBuffer()
+    console.debug(`Writing thumbnail for ${file}`)
+    await writeFile(`${caches}/${fileId}.jpg`, fileBuffer)
+    console.debug(`Finished writing thumbnail for ${file}`)
+    console.debug(`Writing metadata for ${file}`)
+    await createFileEntry(fileStore as FilestoreEntry)
+    console.debug(`Finished writing metadata for ${file}`)
+  }
+}
+migrate(directory)
